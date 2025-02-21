a2c8901ee7e416c205600c169d084388ac3fd906
Quoc Huy <thanhquay113@gmail.com>
2025-02-21 14:49:43 +0700

    chapter 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -16,7 +16,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 export async function createInvoice(formData: FormData) {
   // Alternative: const rawFormData = Object.fromEntries(formData.entries())
   const rawFormData = {
@@ -35,4 +35,26 @@ export async function createInvoice(formData: FormData) {
   // console.log(rawFormData);
   revalidatePath("/dashboards/invoices");
   redirect("/dashboard/invoices");
+}
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
 }
