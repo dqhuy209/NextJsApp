8f02a7a2aaf72817dadaf51eb09fd7ee39bfa39c
Quoc Huy <thanhquay113@gmail.com>
2025-02-21 15:13:14 +0700

    chapter 12+13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,12 +27,10 @@ export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
-
   await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-  // console.log(rawFormData);
   revalidatePath("/dashboards/invoices");
   redirect("/dashboard/invoices");
 }
@@ -44,13 +42,11 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
 
   const amountInCents = amount * 100;
-
   await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-  `;
-
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+    `;
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
